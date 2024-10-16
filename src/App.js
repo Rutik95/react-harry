c32830587e707d3d71c4f47ce22399f25dd2c0f9
Rutik95 <rutik.tarekar@swegon.in>
2024-10-16 17:19:22 +0530

    version beta - Small change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React bhaiiiiiiiiiiiiiiii
+          Learn React bhaiiiiiiiiiiiiiiii lets make changes
         </a>
       </header>
     </div>
