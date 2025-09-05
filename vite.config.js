97227f9219e2cb3f002ee81280a785eb23d70570
Rutik95 <rutik.tarekar@swegon.in>
2025-09-05 16:25:46 +0530

    Clear gh-pages branch

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/react-harry', // Matches https://Rutik95.github.io/react-harry,
-  plugins: [react(),tailwindcss()],
-})
