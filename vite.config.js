ebd0beab682c8536149099c09b18b621d0003183
Rutik95 <rutik.tarekar@swegon.in>
2025-09-05 16:18:56 +0530

    Fix GitHub Pages build with Vite and disable Jekyll

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-harry', // Matches https://Rutik95.github.io/react-harry,
   plugins: [react(),tailwindcss()],
 })
